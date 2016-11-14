5e138d6321b4102548916726dc0b02b3f38b9e8c
xzorcious <simonrosengren123@gmail.com>
2016-11-14 14:54:04 +0100

    Added some content

---
+++ ThirdUnityGitTry/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    GameObject player;
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z - 4);
+	}
+}
---
+++ ThirdUnityGitTry/Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    Rigidbody rb;
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rb.AddForce(new Vector3(0, 0, 1));
+	}
+}
